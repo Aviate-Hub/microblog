97b9abc68e4d03fe9df4671603c17ecdff382842
Nitesh D <niteshd@me.com>
2022-01-03 15:52:40 -0500

    app/templates/base.html: Use url\_for() function for links app/routes.py: Use url\_for() function for links

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 # Created: 01/03/2022
 # Developer: Nitesh D
 # Last Modified: 01/03/2022
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -33,5 +33,5 @@ def login():
 	if form.validate_on_submit():
 		flash('Login requested for user {}, remember_me={}'.format(
 		form.username.data, form.remember_me.data))
-		return redirect('/index')
+		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
