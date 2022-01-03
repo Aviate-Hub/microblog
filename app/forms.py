e4fafef88d2295c9f787b617ba32408dc040de9c
Nitesh D <niteshd@me.com>
2022-01-03 14:27:47 -0500

    forms.py missing "s" under validators

--- app/forms.py
+++ app/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validator=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
