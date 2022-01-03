50c31a940adfbbffc8ab7999428c3b5c51cbe7af
Nitesh D <niteshd@me.com>
2022-01-03 18:53:17 -0500

    Added shell context

--- microblog.py
+++ microblog.py
@@ -4,4 +4,8 @@
 # Last Modified: 01/03/2022
 
 from app import app
+from app.models import User, Post
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
