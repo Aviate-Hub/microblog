50ff6aa4ddf3ee02a03fc03e9c733734ca70425c
Nitesh D <niteshd@me.com>
2022-01-04 11:51:15 -0500

    app/templates/user.html: Show user information in user profile template app/routes.py: Record time of last visit

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -88,3 +89,11 @@ def user(username):
 		{'author': user, 'body': 'Microblog Test post #3'}
 	]
 	return render_template('user.html', user=user, posts=posts)
+
+
+# Record time of users last visit
+@app.before_request
+def before_request():
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow()
+		db.session.commit()
