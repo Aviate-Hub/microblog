3eb7d79e987d1955ebe7d1e59a4c1a7d040c6303
Nitesh D <niteshd@me.com>
2022-01-03 20:43:35 -0500

    app/__init__.py: Flask-Login initialization app/models.py: Flask-Login user mixin class app/models.py: Flask-Login user loader function app/routes.py: Login view function logic app/routes.py: Logout view function app/templates/base.html: Conditional login and logout links app/routes.py: @login\_required decorator app/templates/index.html: Pass current user to template app/routes.py: Do not pass user to template anymore

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,18 @@
 # Created: 01/03/2022
 # Developer: Nitesh D
 # Last Modified: 01/03/2022
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-	user = {'username': 'Nitesh'}
+#	user = {'username': 'Nitesh'}
 	posts = [
 		{
 			'author': {'username': 'John'},
@@ -24,14 +28,34 @@ def index():
 			'body': 'Give me some chickmen!'
 		}
 	]
-	return render_template('index.html', title='Home', user=user, posts=posts)
+	return render_template('index.html', title='Home', posts=posts)
 
 
+# Login user to application
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+			return redirect(url_for('index'))
+
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(
-		form.username.data, form.remember_me.data))
-		return redirect(url_for('index'))
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password.')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = redirect(url_for('index'))
+		return redirect(next_page)
+#		flash('Login requested for user {}, remember_me={}'.format(
+#		form.username.data, form.remember_me.data))
+#		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
+
+
+# Logout user from application
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
