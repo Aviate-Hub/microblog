af983b9bd0c938eb2c8abe59851902c20980605f
Nitesh D <niteshd@me.com>
2022-01-04 15:03:53 -0500

    app/__init__.py: Logging to a file

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,9 @@ login.login_view = 'login'
 
 from app import routes, models, errors
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -36,5 +38,14 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineo)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
