066c2b37489e4ccaa93f76f50e5d0ffba9e96205
Nitesh D <niteshd@me.com>
2022-01-04 14:46:37 -0500

    app/__init__.py: Log errors by email

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,24 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
+import logging
+from logging.handlers import SMTPHandler
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 #if __name__ == '__main__':
 #    app.run(debug=True)
