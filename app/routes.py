ac44234d1a87ad6e7302c6c84714e0a75c5938be
Nitesh D <niteshd@me.com>
2022-01-05 17:42:41 -0500

    To make it easier to find users to follow updates

--- app/routes.py
+++ app/routes.py
@@ -153,3 +153,11 @@ def unfollow(username):
 		return redirect(url_for('user', username=username))
 	else:
 		return redirect(url_for('index'))
+
+
+# Explore view function
+@app.route('/explore')
+@login_required
+def explore():
+	posts = Post.query.order_by(Post.timestamp.desc()).all()
+	return render_template('index.html', title='Explore', posts=posts)
