9d6fadf7f547d389d138938a21daf18159e56a74
Nitesh D <niteshd@me.com>
2022-01-06 12:42:20 -0500

    Page navigation

--- app/routes.py
+++ app/routes.py
@@ -28,7 +28,14 @@ def index():
 #	posts = current_user.followed_posts().all()
 	posts = current_user.followed_posts().paginate(
 		page, app.config['POSTS_PER_PAGE'], False)
-	return render_template('index.html', title='Home', form=form, posts=posts.items)
+	next_url = url_for('index', page=posts.next_num) \
+		if posts.has_next else None
+	prev_url = url_for('index', page=posts.prev_num) \
+		if posts.has_prev else None
+
+	return render_template('index.html', title='Home', form=form,
+                        posts=posts.items, next_url=next_url,
+                        prev_url=prev_url)
 
 
 # Login user to application
@@ -166,4 +173,10 @@ def explore():
 #	posts = Post.query.order_by(Post.timestamp.desc()).all()
 	posts = Post.query.order_by(Post.timestamp.desc()).paginate(
 		page, app.config['POSTS_PER_PAGE'], False)
-	return render_template('index.html', title='Explore', posts=posts.items)
+	next_url = url_for('explore', page=posts.next_num) \
+		if posts.has_next else None
+	prev_url = url_for('explore', page=posts.prev_num) \
+		if posts.has_prev else None
+
+	return render_template('index.html', title='Explore', posts=posts.items,
+                        next_url=next_url, prev_url=prev_url)
