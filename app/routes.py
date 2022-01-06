01173630205bf66d9d4793dc78cf8a5e303f6287
Nitesh D <niteshd@me.com>
2022-01-06 12:22:59 -0500

    Paginate

--- app/routes.py
+++ app/routes.py
@@ -24,8 +24,10 @@ def index():
 		db.session.commit()
 		flash('Your post is now live!')
 		return redirect(url_for('index'))
-	posts = current_user.followed_posts().all()
-	return render_template('index.html', title='Home', form=form, posts=posts)
+	page = request.args.get('page', 1, type=int)
+#	posts = current_user.followed_posts().all()
+	posts = current_user.followed_posts().paginate(page, app.config['POST_PER_PAGE'], False)
+	return render_template('index.html', title='Home', form=form, posts=posts.items)
 
 
 # Login user to application
@@ -159,5 +161,7 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-	posts = Post.query.order_by(Post.timestamp.desc()).all()
-	return render_template('index.html', title='Explore', posts=posts)
+	page = request.args.get('page', 1, type=int)
+#	posts = Post.query.order_by(Post.timestamp.desc()).all()
+	posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POST_PER_PAGE'], False)
+	return render_template('index.html', title='Explore', posts=posts.items)
