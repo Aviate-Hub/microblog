92f87659b5c93ee5b4cddc1f7ed900dd12710de8
Nitesh D <niteshd@me.com>
2022-01-06 13:30:35 -0500

    Adding Flask-Mail instance

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,16 @@
 # Developer: Nitesh D
 # Last Modified: 01/03/2022
 
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import logging
+from app import routes, models, errors
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,11 +20,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
-from app import routes, models, errors
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
-import os
+mail = Mail(app)
 
 
 if not app.debug:
@@ -41,9 +42,10 @@ if not app.debug:
 if not app.debug:
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler = RotatingFileHandler(
+        'logs/microblog.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.Formatter(
-    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
