782296edbaf9abf8d72876788bcac634fc14ced6
Nitesh D <niteshd@me.com>
2022-01-06 14:02:57 -0500

    Bug fix

--- app/routes.py
+++ app/routes.py
@@ -90,14 +90,15 @@ def register():
 def user(username):
 	user = User.query.filter_by(username=username).first_or_404()
 	page = request.args.get('page', 1, type=int)
-	posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+	posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+		page, app.config['POSTS_PER_PAGE'], False)
 	next_url = url_for('user', username=user.username, page=posts.next_num) \
 		if posts.has_next else None
 	prev_url = url_for('user', username=user.username, page=posts.prev_num) \
 		if posts.has_prev else None
 	form = EmptyForm()
 	return render_template('user.html', user=user, posts=posts.items,
-						next_url=next_url, prev_url=prev_url, form=form)
+                        next_url=next_url, prev_url=prev_url, form=form)
 
 
 # Record time of users last visit
