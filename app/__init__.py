0b6c25350121b2343a4b11729d04d68e9859a133
Nitesh D <niteshd@me.com>
2022-01-07 15:23:13 -0500

    old buggy code

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,17 @@
 # Developer: Nitesh D
 # Last Modified: 01/03/2022
 
+
+from app import routes, models, errors
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import logging
-from app import routes, models, errors
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_mail import Mail
+# from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,7 +21,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-mail = Mail(app)
+# mail = Mail(app)
 
 
 if not app.debug:
